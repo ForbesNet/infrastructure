0b24677fc9d9692ae2900f2e29fc6be1944e9886
coteforbes <46688751+coteforbes@users.noreply.github.com>
2025-05-31 19:54:56 -0400

    Delete modules/pihole directory

--- modules/pihole/main.tf
+++
@@ -1,6 +0,0 @@
-# Provider info in root module config
-
-resource "pihole_dns_record" "record" {
-  domain = var.dns_record
-  ip     = var.ip
-}
--- modules/pihole/providers.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_providers {
-    pihole = {
-      source = "ryanwholey/pihole"
-    }
-  }
-}

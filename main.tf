c37692a44df2e70273254aeaa235ac59108d3678
Cote <cote@Cotes-MBP.lan>
2025-05-31 20:15:00 -0400

    fix mistakes hopefully

--- main.tf
+++ main.tf
@@ -24,3 +24,19 @@ module "pihole_dns_record" {
     dns_record = "${each.key}.forbesnet.local"
     ip = each.value.ip
 }
+
+
+module "forbesnet_linux_vm" {
+  source   = "./modules/forbesnet_linux_vm" # If referencing GHE
+  for_each = var.linux_vms
+
+    vm_name            = each.key
+    num_cpus           = each.value.num_cpus
+    memory             = each.value.memory
+    ipv4_address       = each.value.ipv4_address
+    vsphere_template   = each.value.vsphere_template
+    vsphere_datacenter = each.value.vsphere_datacenter
+    vsphere_datastore  = each.value.vsphere_datastore
+    vsphere_cluster    = each.value.vsphere_cluster
+    vsphere_network    = each.value.vsphere_network
+}
